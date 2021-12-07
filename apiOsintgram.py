822c608630ede4e77b31076fa7b61670a8358600
Sergi <sergidoblado#@iesebre.com>
2021-12-07 19:46:43 +0100

    WIP

--- apiOsintgram.py
+++ apiOsintgram.py
@@ -28,6 +28,10 @@ def info():
     usuari = input("Disme l'usuari d'Instagram del qual vols obtenir informació: ")
     subprocess.call("cd Osintgram && python3 ./main.py {} -c info".format(usuari), shell=True)
 
+def fwingsnumber():
+    usuari = input("Disme l'usuari d'Instagram del qual vols obtenir el número de telèfon dels usuaris seguits de l'objectiu: ")
+    subprocess.call("cd Osintgram && python3 ./main.py {} -c fwingsnumber".format(usuari), shell=True
+    
 def demanaNumeroEnter():
     correcto=False
     num=0
@@ -51,7 +55,8 @@ while not salir:
     print ("Opció 4. Obtenir totes les fotos registrades dirigides a les fotos de destinació")
     print ("Opció 5. Obteniu el correu electrònic dels seguidors de l'objectiu")
     print ("Opció 6. Obtenir l'informació de l'objectiu")
-    print ("7. Sortir")
+    print ("Opció 7. Obtenir el número de telèfon dels usuaris seguits de l'objectiu:")    
+    print ("8. Sortir")
     print ("------------------------")
 
     print ("Escull una opció")
@@ -62,22 +67,16 @@ while not salir:
         print(" ")
         print("Opcions OSINTGRAM: ")
         print(" ")
-        print(" - addrs           Obteniu totes les fotos registrades dirigides a les fotos de destinació       ")
         print(" - captions        Obteniu subtítols de les fotos de l'usuari       ")
         print(" - comments        Obteniu comentaris totals de les publicacions de l'objectiu   ")
         print(" - followers       Aconsegueix seguidors objectiu")
         print(" - followings      Aconsegueix que els usuaris segueixin l'objectiu")
-        print(" - fwersemail      Obteniu el correu electrònic dels seguidors objectiu")
         print(" - fwingsemail     Obteniu el correu electrònic dels usuaris seguits per l'objectiu")
-        print(" - fwersnumber     Obteniu el número de telèfon dels seguidors objectiu")
-        print(" - fwingsnumber    Obteniu el número de telèfon dels usuaris seguit de l'objectiu")
+        print(" - fwersnumber     Obteniu el número de telèfon dels seguidors objectiu")       
         print(" - hashtags        Obteniu hashtags utilitzats per l'objectiu")
-        print(" - info            Obteniu informació de l'objectiu")
         print(" - likes           Aconsegueix el total de M'agrada de les publicacions de l'objectiu")
         print(" - mediatype       Obteniu el tipus de publicacions de l'usuari (foto o vídeo)")
         print(" - photodes        Obteniu una descripció de les fotos de l'objectiu ")
-        print(" - photos          Baixeu les fotos de l'usuari a la carpeta de sortida")
-        print(" - propic          Descarrega la foto de perfil de l'usuari")
         print(" - stories         Descarregar històries d'usuari ")
         print(" - tagged          Obteniu una llista d'usuaris etiquetats per objectiu")
         print(" - wcommented      Obteniu una llista dels usuaris que han comentat les fotos de l'objectiu")
@@ -94,6 +93,8 @@ while not salir:
     elif opcion == 6:
         info()
     elif opcion == 7:
+        fwingsnumber()
+    elif opcion == 8:
         salir = True
     else:
         print ("Introduce un numero entre 1 y 7")
