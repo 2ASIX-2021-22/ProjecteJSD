9ea0e730184ce2275f0f758709fab469634c411a
alumne <jamalbourigue@iesebre.com>
2021-12-09 19:50:06 +0100

    WIP

--- uDork.py
+++ uDork.py
@@ -6,7 +6,7 @@ def pedirNumeroEntero():
     num=0
     while(not correcto):
         try:
-            num = int(input("Introdueix un numero de l'1 al 3: "))
+            num = int(input("Introdueix un numero de l'1 al 4: "))
             correcto=True
         except ValueError:
             print('Error, Introdueix una opcio del menú:')
@@ -32,18 +32,22 @@ def menu_ajuda():
 def menu_execucio():
     print ("Opció 2")
     domini=input("introdueix un nom de un domini: ")
-    extensio=input("introdueix la extensio dels fitxers que vols buscar (all=totes), : ")
+    extensio=input("introdueix la extensio dels fitxers que vols buscar (all, pdf, ...) : ")
     subprocess.call("cd uDork &&  ./uDork.sh {} -e {}".format(domini, extensio), shell=True)
+def menu_avançat():
+    print ("Opció 3")
+    domini=input("introdueix un nom de un domini: ")
+    pentesting=input("introdueix on vols fer la auditories/pentesting (admin, directories, usernames, passwords, webservers, vulnerable_files, vulnerable_servers, error_messages, portal_logins, vulnerable_networks, devices : ")
+    subprocess.call("cd uDork/ && ./uDork.sh {} -g {}".format(domini, pentesting), shell=True)
 
 while not salir:
     print ("------------------------")
     print ("Opció 1. Ajuda")
     print ("Opció 2. Execució")
-    print ("Opció 3. Sortir")
+    print ("Opció 3. Auditoria")
+    print ("Opció 4. Sortir")
     print ("------------------------")
     print ("Escull una opció")
-    
-
     opcion = pedirNumeroEntero()
     
     if opcion == 1:
@@ -51,8 +55,10 @@ while not salir:
     elif opcion == 2:
         menu_execucio()
     elif opcion == 3:
+        menu_avançat()
+    elif opcion == 4:
         salir = True
     else:
-        print ("Introduce un numero entre 1 y 3")
+        print ("Introduce un numero entre 1 y 4")
  
 print ("Fi")
