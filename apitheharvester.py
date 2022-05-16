3164567ce3036498969cf0f9b51f234d255e5f03
David Estrada <davidestrada@iesebre.com>
2022-05-16 19:42:37 +0200

    WIP

--- apitheharvester.py
+++ apitheharvester.py
@@ -13,10 +13,10 @@ def demanaNumeroEnter():
     num=0
     while(not correcto):
         try:
-            num = int(input("Introdueix un numero de l'1 al 3: "))
+            num = int(input("    Introdueix un número de l'1 al 3: "))
             correcto=True
         except ValueError:
-            print('Error, Introdueix una opcio del menú:')
+            print('    Error, introdueix una opcio del menú:')
 
     return num
 
@@ -24,32 +24,33 @@ salir = False
 opcion = 0
 
 while not salir:
-    print ("------------------------")
-    print ("Opció 1. Execució")
-    print ("Opció 2. Mostrar resultats:")
-    print ("3. Tornar al menú")
-    print ("------------------------")
+    print ("    ------------------------")
+    print ("    Opció 1. Execució")
+    print ("    Opció 2. Mostra i envia els resultats")
+    print ("    Opció 3. Tornar al menú")
+    print ("    ------------------------")
 
-    print ("Escull una opció")
+    print ("    Escull una opció")
 
     opcion = demanaNumeroEnter()
 
     if opcion == 1:
-      domain = input("Disme el domini que vols buscar: ")
-      limit = input("Disme el limit de resultats: ")
-      fontDades = input("Disme la font a la qual farem la busqueda: (google, googleCSE, bing, bingapi, pgp, linkedin, google-profile, people123, jigsaw, twitter, googleplus, all)")
+      domain = input("    Disme el domini que vols buscar: ")
+      limit = input("    Disme el limit de resultats: ")
+      fontDades = "google"
       subprocess.call("cd theHarvester && python3 ./theHarvester.py -d {} -l {} -b {} -f resulttheharvester.html".format(domain, limit, fontDades), shell=True)
     elif opcion == 2:
-        print("Mostrar resultats al navegador: ")
+        print("    Mostrar resultats al navegador: ")
         url=("./theHarvester/resulttheharvester.xml")
-        enviarDocumento("/home/alumne/Code/ProjecteJSD/theHarvester/resulttheharvester.xml")
+        enviarDocumento("./theHarvester/resulttheharvester.xml")
         chrome_path = '/usr/bin/google-chrome %s'
         webbrowser.get(chrome_path).open(url)     
     elif opcion == 3:
         salir = True
     else:
-        print ("Introduce un numero entre 1 y 3")
+        print ("    Introdueix un número entre l'1 i el 3")
 
-print ("Fi")
+print ("    Menú principal: ")
+exec(open("main.py").read())
 
 
