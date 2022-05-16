5a587991c20644266720f7349417cfb6f882e863
jamal <jamalbourigue@iesebre.com>
2022-05-16 19:05:17 +0200

    WIP

--- apiOsintgram.py
+++ apiOsintgram.py
@@ -4,6 +4,13 @@
 import asyncio
 import subprocess
 from bot_telegram import enviarFotos, enviarMensaje, enviarDocumento
+def credentials():
+#    usuari = input("Disme l'usuari de l'Instagram: ")
+#    password = input("Disme la contrasenya de l'usuari de l'Instagram: ")
+    subprocess.call("cd ./Osintgram && make setup", shell=True)
+credentials()
+
+    
 def propic():
     usuari = input("    Disme l'usuari d'Instagram que vols buscar: ")
     subprocess.call("cd Osintgram && python3 ./main.py {} -c propic".format(usuari), shell=True)
@@ -34,20 +41,20 @@ def fwersemail():
     subprocess.call("cd Osintgram/output/ && rm -rf ./*", shell=True)
 
 def info():
-    usuari = input("Disme l'usuari d'Instagram del qual vols obtenir informació: ")
+    usuari = input("    Disme l'usuari d'Instagram del qual vols obtenir informació: ")
     subprocess.call("cd Osintgram && python3 ./main.py {} -c info -f".format(usuari), shell=True)
     enviarMensaje("Info")
     subprocess.call("cd Osintgram/output/ && rm -rf ./*", shell=True)
 
 def fwingsnumber():
-    usuari = input("Disme l'usuari d'Instagram del qual vols obtenir el número de telèfon dels usuaris seguits de l'objectiu: ")
+    usuari = input("    Disme l'usuari d'Instagram del qual vols obtenir el número de telèfon dels usuaris seguits de l'objectiu: ")
     subprocess.call("cd Osintgram && python3 ./main.py {} -c fwingsnumber -f".format(usuari), shell=True)
     enviarMensaje("Numeros de mobil")
     enviarDocumento("/app/Osintgram/output/{}_fwingsnumber.txt".format(usuari))
     subprocess.call("cd Osintgram/output/ && rm -rf ./*", shell=True)
 
 def followings():
-    usuari = input("Disme l'usuari d'Instagram del qual vols obtenir el nom dels usuaris seguits de l'objectiu: ")
+    usuari = input("    Disme l'usuari d'Instagram del qual vols obtenir el nom dels usuaris seguits de l'objectiu: ")
     subprocess.call("cd Osintgram && python3 ./main.py {} -c followings -f".format(usuari), shell=True)
     enviarMensaje("Nom d'usuari")
     enviarDocumento("/app/Osintgram/output/{}_followings.txt".format(usuari))
@@ -58,10 +65,10 @@ def demanaNumeroEnter():
     num=0
     while(not correcte):
         try:
-            num = int(input("Introdueix un numero de l'1 al 8: "))
+            num = int(input("    Introdueix un numero de l'1 al 8: "))
             correcte=True
         except ValueError:
-            print('Error, Introdueix una opcio del menú:')
+            print('    Error, Introdueix una opcio del menú:')
 
     return num
 
@@ -69,60 +76,42 @@ sortir = False
 opcion = 0
 
 while not sortir:
-    print ("------------------------")
-    print ("Opció 1. Opcions OSINTGRAM")
-    print ("Opció 2. Veure foto perfil Instagram:")
-    print ("Opció 3. Veure fotos públicades a Instagram:")
-    print ("Opció 4. Obtenir totes les fotos registrades dirigides a les fotos de destinació")
-    print ("Opció 5. Obteniu el correu electrònic dels seguidors de l'objectiu")
-    print ("Opció 6. Obtenir l'informació de l'objectiu")
-    print ("Opció 7. Obtenir el número de telèfon dels usuaris seguits de l'objectiu:")    
-    print ("Opció 8. Obtenir el nom d'usuari dels usuaris seguits de l'objectiu:")    
-    print ("9. Sortir")
-    print ("------------------------")
+    print ("    ------------------------")
+    print ("    Opció 1. Veure foto perfil Instagram:")
+    print ("    Opció 2. Veure fotos públicades a Instagram:")
+    print ("    Opció 3. Obtenir totes les fotos registrades dirigides a les fotos de destinació")
+    print ("    Opció 4. Obteniu el correu electrònic dels seguidors de l'objectiu")
+    print ("    Opció 5. Obtenir l'informació de l'objectiu")
+    print ("    Opció 6. Obtenir el número de telèfon dels usuaris seguits de l'objectiu:")    
+    print ("    Opció 7. Obtenir el nom d'usuari dels usuaris seguits de l'objectiu:")    
+    print ("    Opció 8. Tornar al menú principal:")
+    print ("    ------------------------")
 
-    print ("Escull una opció")
+    print ("    Escull una opció: ")
 
     opcion = demanaNumeroEnter()
 
     if opcion == 1:
-        print(" ")
-        print("Opcions OSINTGRAM: ")
-        print(" ")
-        print(" - captions        Obteniu subtítols de les fotos de l'usuari       ")
-        print(" - comments        Obteniu comentaris totals de les publicacions de l'objectiu   ")
-        print(" - followers       Aconsegueix seguidors objectiu")
-        print(" - followings      Aconsegueix que els usuaris segueixin l'objectiu")
-        print(" - hashtags        Obteniu hashtags utilitzats per l'objectiu")
-        print(" - likes           Aconsegueix el total de M'agrada de les publicacions de l'objectiu")
-        print(" - mediatype       Obteniu el tipus de publicacions de l'usuari (foto o vídeo)")
-        print(" - photodes        Obteniu una descripció de les fotos de l'objectiu ")
-        print(" - stories         Descarregar històries d'usuari ")
-        print(" - tagged          Obteniu una llista d'usuaris etiquetats per objectiu")
-        print(" - wcommented      Obteniu una llista dels usuaris que han comentat les fotos de l'objectiu")
-        print(" - wtagged         Obteniu una llista d'usuaris que han etiquetat l'objectiu")
-
-    elif opcion == 2:
         propic()
-    elif opcion == 3:
+    elif opcion == 2:
         photos()
-    elif opcion == 4:
+    elif opcion == 3:
         addrs()
-    elif opcion == 5:
+    elif opcion == 4:
         fwersemail()
-    elif opcion == 6:
+    elif opcion == 5:
         info()
-    elif opcion == 7:
+    elif opcion == 6:
         fwingsnumber()
-    elif opcion == 8:
+    elif opcion == 7:
         followings()
-    elif opcion == 9:
+    elif opcion == 8:
         sortir = True
     else:
-        print ("Introdueix un numero entre 1 y 7")
+        print ("    Introdueix un número entre l'1 i 8")
 
 
-print ("Fi osintgram")
+print ("    Menú principal: ")
 exec(open("main.py").read())
 
 
